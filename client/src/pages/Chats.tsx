50be049819410840f6ea6667f8c2f7175289f3ea
Ansilaju <ansilajbin@gmail.com>
2025-01-01 14:43:43 +0530

    changes in chats

--- client/src/pages/Chats.tsx
+++ client/src/pages/Chats.tsx
@@ -2,8 +2,9 @@ const Chats = () => {
   return (
     <div>
       <h1>Welcome to the Chats Page</h1>
+      <button className="text-red-500">chat</button>
     </div>
-  )
-}
+  );
+};
 
-export default Chats
+export default Chats;
