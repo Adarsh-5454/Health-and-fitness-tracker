355a95bc94f05f13c41577fe838203e0442f8b65
Athira-p-g <athirabkm@gmail.com>
2025-01-01 14:36:02 +0530

    shopping

---
+++ client/src/pages/Shopping.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shopping = () => {
+  return (
+    <div
+      className="font-semibold
+"
+    >
+      hello world
+    </div>
+  );
+};
+
+export default Shopping;
