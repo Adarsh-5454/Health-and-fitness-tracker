b67712056305704e97bc0a2e8f05ba371f4be8ed
Athira-p-g <athirabkm@gmail.com>
2025-01-02 16:09:13 +0530

    add icons in shopping

--- client/src/components/shopping/NavBarShop/NavBarShop.tsx
+++ client/src/components/shopping/NavBarShop/NavBarShop.tsx
@@ -1,55 +1,41 @@
 import React from "react";
 import styles from "./NavBarShop.module.css";
+import { FiSearch } from "react-icons/fi";
+import { FaCircleUser } from "react-icons/fa6";
+import { GoHeartFill } from "react-icons/go";
+import { FaShoppingCart } from "react-icons/fa";
 
 const NavBarShop = () => {
-   return (
-      <>
-         <div className={styles.NavSection}>
-            <div className={styles.NavLeft}>
-               <img
-                  src="src\assets\shopping\loder.png.webp"
-                  alt="oops"
-                  className={styles.logo}
-               />
-               <h3 className={styles.logoName}>FIT MAESTRO</h3>
-            </div>
-            <div className={styles.NavRight}>
-               <button className={styles.icon}>
-                  <img
-                     src="src\assets\shopping\search.jpeg"
-                     alt="oops"
-                     className={styles.img}
-                  />
-               </button>
+  return (
+    <>
+      <div className={styles.NavSection}>
+        <div className={styles.NavLeft}>
+          <img
+            src="..\..\src\assests\shopping\loder.png.webp"
+            alt="oops"
+            className={styles.logo}
+          />
+          <h3 className={styles.logoName}>FIT MAESTRO</h3>
+        </div>
+        <div className={styles.NavRight}>
+          <button className={styles.icon}>
+            <FiSearch />
+          </button>
+          <button className={styles.icon}>
+            <FaCircleUser />
+          </button>
 
-               <button className={styles.icon}>
-                  <img
-                     src="src\assets\shopping
-                     profile.jpeg"
-                     alt="oops"
-                     className={styles.img}
-                  />
-               </button>
+          <button className={styles.icon}>
+            <GoHeartFill />
+          </button>
 
-               <button className={styles.icon}>
-                  <img
-                     src="src\assets\shopping\heart.jpeg"
-                     alt="oops"
-                     className={styles.img}
-                  />
-               </button>
-
-               <button className={styles.icon}>
-                  <img
-                     src="src\assets\cart.jpeg"
-                     alt="oops"
-                     className={styles.img}
-                  />
-               </button>
-            </div>
-         </div>
-      </>
-   );
+          <button className={styles.icon}>
+            <FaShoppingCart />
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NavBarShop;
