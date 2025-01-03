f2b732d5fd0b763c9a82017d7927899045e4cc66
abhishekvijayan123 <abhishekvijayan20@gmail.com>
2025-01-03 10:55:37 +0530

    changes in blog page

--- client/src/pages/Blogs.tsx
+++ client/src/pages/Blogs.tsx
@@ -24,7 +24,7 @@ function Blogs() {
               spellCheck="false"
               className="border border-gray-300 outline-none bg-gray-50 text-gray-700 px-4 py-3 h-14 rounded-full flex-1 mr-4 text-lg"
             />
-            <button className="border-none outline-none bg-gray-50 rounded-full w-14 h-14 cursor-pointer ">
+            <button className="border-none outline-none bg-gray-50 rounded-full w-14 h-14 cursor-pointer  flex justify-center items-center">
               <img src="src\assets\blogs\search.png" alt="" className="w-4" />
             </button>
           </div>
