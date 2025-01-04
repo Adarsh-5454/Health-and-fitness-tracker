49c8cbcfddd80491532feab0d7632fef68363518
JithuPrabhakar <join.jitz@gmail.com>
2025-01-04 16:23:28 +0530

    readjusts navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,24 +1,19 @@
 import { RiChatAiFill } from "react-icons/ri";
 import { FaCircleUser } from "react-icons/fa6";
 const Navbar = () => {
-   return (
-      <div className="fixed w-full py-2 bg-transparent top-0 left-0 right-0 z-1 flex justify-between px-16">
-         <div className="flex items-center gap-4">
-            <button className="w-8 h-8 bg-secondary-dark rounded-full"></button>
-            <h3 className="font-bold text-xl text-purple-700">FIT MAESTRO</h3>
-         </div>
-
-         <div className="flex items-center gap-4">
-            <button className="w-5 h-5 text-3xl text-black rounded-full">
-               <RiChatAiFill />
-            </button>
-
-            <button className="w-5 h-5 text-3xl text-black rounded-full">
-               <FaCircleUser />
-            </button>
-         </div>
+  return (
+    <div className="fixed py-4 bg-transparent top-0 left-0 right-0 z-1 flex justify-between px-24">
+      <div className="flex items-center gap-6">
+        <div className="w-8 h-8 bg-secondary-light rounded-full"></div>
+        <h3 className="font-bold text-xl text-primary-light">FIT MAESTRO</h3>
       </div>
-   );
+
+      <div className="flex items-center gap-6">
+        <RiChatAiFill className="text-3xl text-secondary-light rounded-full" />
+        <FaCircleUser className="text-3xl text-secondary-light rounded-full" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
