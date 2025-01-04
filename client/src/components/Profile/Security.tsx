885149f008854135c1acd37ca14cc029c117d672
Christo-Jacob <christojacob28@gmail.com>
2025-01-04 16:46:28 +0530

    changes in Profile

--- client/src/components/Profile/Security.tsx
+++ client/src/components/Profile/Security.tsx
@@ -5,34 +5,34 @@ import LabelInput from "./EditProfile/LabelInput";
 import SaveCancel from "./SaveCancel";
 
 const Security = () => {
-  return (
-    <>
-      <div className="flex flex-col  w-full px-36">
-        <div className="text-center text-3xl font-serif mb-12 ">
-          <h2>CHANGE PASSWORD</h2>
-        </div>
-        <LabelInput
-          placeholder={"Enter Current Password"}
-          type={"text"}
-          label={"Current Password"}
-        />
-        <div className="flex gap-2 w-full">
-          <LabelInput
-            placeholder={"Enter New Password "}
-            type={"text"}
-            label={"Enter New Password"}
-          />
-          <LabelInput
-            placeholder={"Confirm Password "}
-            type={"text"}
-            label={"Confirm Password"}
-          />
-        </div>
+   return (
+      <>
+         <div className="flex flex-col  mt-5 mt-5  w-full px-36">
+            <div className="text-center text-3xl font-serif mb-12 ">
+               <h2>CHANGE PASSWORD</h2>
+            </div>
+            <LabelInput
+               placeholder={"Enter Current Password"}
+               type={"text"}
+               label={"Current Password"}
+            />
+            <div className="flex gap-2 w-full">
+               <LabelInput
+                  placeholder={"Enter New Password "}
+                  type={"text"}
+                  label={"Enter New Password"}
+               />
+               <LabelInput
+                  placeholder={"Confirm Password "}
+                  type={"text"}
+                  label={"Confirm Password"}
+               />
+            </div>
 
-        <SaveCancel />
-      </div>
-    </>
-  );
+            <SaveCancel />
+         </div>
+      </>
+   );
 };
 
 export default Security;
