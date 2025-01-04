6fb77ef536be15d00d3a0bb8b6051c1bbce19550
JithuPrabhakar <join.jitz@gmail.com>
2025-01-04 16:28:23 +0530

    changes navbar to color in other pages

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,19 @@
 import { RiChatAiFill } from "react-icons/ri";
 import { FaCircleUser } from "react-icons/fa6";
+import { useLocation } from "react-router-dom";
+
 const Navbar = () => {
+  const location = useLocation();
+
+  // Check if the current path is the home page
+  const isHomePage = location.pathname === "/";
+
   return (
-    <div className="fixed py-4 bg-transparent top-0 left-0 right-0 z-1 flex justify-between px-24">
+    <div
+      className={`fixed py-4 top-0 left-0 right-0 z-10 flex justify-between px-24 ${
+        isHomePage ? "bg-transparent" : "bg-primary-dark"
+      }`}
+    >
       <div className="flex items-center gap-6">
         <div className="w-8 h-8 bg-secondary-light rounded-full"></div>
         <h3 className="font-bold text-xl text-primary-light">FIT MAESTRO</h3>
