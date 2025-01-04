0f34a5bb7d845012067496ebdb5dd3b669dbcb49
SreekuttanCS <sreekuttancs26@gmail.com>
2025-01-04 12:30:45 +0530

    changes in Button color and ProfileNav

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Button = ({ label }) => {
-   return (
-      <>
-         <button className="py-2 px-5 w-36 mb-6 text-lg font-bold border-primary-dark text-white rounded-md bg-primary-light hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-purple-500">
-            {label}
-         </button>
-      </>
-   );
+  return (
+    <>
+      <button className="py-2 px-5  mb-6 text-lg font-bold border border-1 border-primary-dark text-white rounded-md bg-primary-dark hover:bg-secondary-light hover:text-primary-dark focus:outline-none focus:ring-2 focus:ring-purple-500">
+        {label}
+      </button>
+    </>
+  );
 };
 
 export default Button;
--- client/src/components/Profile/ProfileNav.tsx
+++ client/src/components/Profile/ProfileNav.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import Button from "../Button/Button";
 
 const ProfileNav = () => {
-   return (
-      <>
-         <div className="flex flex-col items-center p-5 w-56 h-screen border-e-2 border-slate-600">
-            <div className="w-36 h-36 bg-black rounded-full bg-cover bg-center mb-8"></div>
-            <Button label={"Edit Profile"} />
-            <Button label={"Security"} />
-            <Button label={"Role"} />
-            <Button label={"Logout"} />
-         </div>
-      </>
-   );
+  return (
+    <>
+      <div className="flex flex-col  p-5 w-56 h-screen border-e-2 border-slate-600">
+        <div className="w-36 mx-auto h-36 bg-black rounded-full bg-cover bg-center mb-8"></div>
+        <Button label={"Edit Profile"} />
+        <Button label={"Security"} />
+        <Button label={"Role"} />
+        <Button label={"Logout"} />
+      </div>
+    </>
+  );
 };
 
 export default ProfileNav;
