c3f37f8dc65be5cf7e8cd2dc0034c4dc2391bccd
Ansilaju <ansilajbin@gmail.com>
2025-01-04 12:11:08 +0530

    Changes in profile security

--- client/src/components/Profile/Security.tsx
+++ client/src/components/Profile/Security.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Input from "../Inputbox/Input";
+import Button from "../Button/Button";
+import LabelInput from "./EditProfile/LabelInput";
+import SaveCancel from "./SaveCancel";
 
 const Security = () => {
-   return <div>Security</div>;
+  return (
+    <>
+      <div className="flex flex-col  w-full px-36">
+        <div className="text-center text-3xl font-serif mb-12 ">
+          <h1>CHANGE PASSWORD</h1>
+        </div>
+        <LabelInput
+          placeholder={"Enter Current Password"}
+          type={"text"}
+          label={"Current Password"}
+        />
+        <div className="flex gap-2 w-full">
+          <LabelInput
+            placeholder={"Enter New Password "}
+            type={"text"}
+            label={"Enter New Password"}
+          />
+          <LabelInput
+            placeholder={"Confirm Password "}
+            type={"text"}
+            label={"Confirm Password"}
+          />
+        </div>
+
+        <SaveCancel />
+      </div>
+    </>
+  );
 };
 
 export default Security;
