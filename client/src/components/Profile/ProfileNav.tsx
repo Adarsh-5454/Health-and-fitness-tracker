3e49e06e9f6859c61eefbe8b8e159fce5f0988a7
Christo-Jacob <christojacob28@gmail.com>
2025-01-05 13:25:17 +0530

    changes in Profile

--- client/src/components/Profile/ProfileNav.tsx
+++ client/src/components/Profile/ProfileNav.tsx
@@ -2,39 +2,45 @@ import React from "react";
 import Button from "../Button/Button";
 
 const ProfileNav = ({
-  setIsProfile,
-  isProfile,
-  isSecurity,
-  setIsSecurity,
-  isRole,
-  setIsRole,
+   setIsProfile,
+   isProfile,
+   isSecurity,
+   setIsSecurity,
+   isRole,
+   setIsRole,
 }) => {
-  const handleEditProfile = () => {
-    setIsProfile(true);
-  };
-  const handleSecurity = () => {
-    setIsSecurity(true);
-  };
-  const handleRole = () => {
-    setIsRole(true);
-  };
-  return (
-    <>
-      <div className="flex flex-col mt-5  p-5 w-56 h-screen border-e-2 border-slate-600">
-        <div className="w-36 mx-auto h-36 bg-black rounded-full bg-cover bg-center mb-8"></div>
-        <span onClick={handleEditProfile}>
-          <Button label={"Edit Profile"} />
-        </span>
-        <span onClick={handleSecurity}>
-          <Button label={"Security"} />
-        </span>
-        <span onClick={handleRole}>
-          <Button label={"Role"} />
-        </span>
-        <Button label={"Logout"} />
-      </div>
-    </>
-  );
+   const handleEditProfile = () => {
+      setIsProfile(true);
+      setIsSecurity(false);
+      setIsRole(false);
+   };
+   const handleSecurity = () => {
+      setIsSecurity(true);
+      setIsProfile(false);
+      setIsRole(false);
+   };
+   const handleRole = () => {
+      setIsRole(true);
+      setIsSecurity(false);
+      setIsProfile(false);
+   };
+   return (
+      <>
+         <div className="flex flex-col mt-5  p-5 w-56 h-screen border-e-2 border-slate-600">
+            <div className="w-36 mx-auto h-36 bg-black rounded-full bg-cover bg-center mb-8"></div>
+            <span onClick={handleEditProfile}>
+               <Button label={"Edit Profile"} />
+            </span>
+            <span onClick={handleSecurity}>
+               <Button label={"Security"} />
+            </span>
+            <span onClick={handleRole}>
+               <Button label={"Role"} />
+            </span>
+            <Button label={"Logout"} />
+         </div>
+      </>
+   );
 };
 
 export default ProfileNav;
--- client/src/components/Profile/Security.tsx
+++ client/src/components/Profile/Security.tsx
@@ -7,7 +7,7 @@ import SaveCancel from "./SaveCancel";
 const Security = () => {
    return (
       <>
-         <div className="flex flex-col  mt-5 mt-5  w-full px-36">
+         <div className="flex flex-col mt-8  mt-5 mt-5  w-full px-36">
             <div className="text-center text-3xl font-serif mb-12 ">
                <h2>CHANGE PASSWORD</h2>
             </div>
