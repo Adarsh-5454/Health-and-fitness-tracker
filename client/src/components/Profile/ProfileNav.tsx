d86b0655151fc41faad8dca940471f5fd44795fa
Christo-Jacob <christojacob28@gmail.com>
2025-01-05 12:54:57 +0530

    create states in Profile

--- client/src/components/Profile/ProfileNav.tsx
+++ client/src/components/Profile/ProfileNav.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 import Button from "../Button/Button";
 
-const ProfileNav = ({ setIsProfile, isProfile }) => {
-  const handleEditProfile = () => {
-    setIsProfile(true);
-  };
-  return (
-    <>
-      <div className="flex flex-col mt-5  p-5 w-56 h-screen border-e-2 border-slate-600">
-        <div className="w-36 mx-auto h-36 bg-black rounded-full bg-cover bg-center mb-8"></div>
-        <span onClick={handleEditProfile}>
-          <Button label={"Edit Profile"} />
-        </span>
-        <Button label={"Security"} />
-        <Button label={"Role"} />
-        <Button label={"Logout"} />
-      </div>
-    </>
-  );
+const ProfileNav = ({ setIsProfile, isProfile, isSecurity, setIsSecurity }) => {
+   const handleEditProfile = () => {
+      setIsProfile(true);
+   };
+   const handleSecurity = () => {
+      setIsSecurity(true);
+   };
+   return (
+      <>
+         <div className="flex flex-col mt-5  p-5 w-56 h-screen border-e-2 border-slate-600">
+            <div className="w-36 mx-auto h-36 bg-black rounded-full bg-cover bg-center mb-8"></div>
+            <span onClick={handleEditProfile}>
+               <Button label={"Edit Profile"} />
+            </span>
+            <span onClick={handleSecurity}>
+               <Button label={"Security"} />
+            </span>
+            <Button label={"Role"} />
+            <Button label={"Logout"} />
+         </div>
+      </>
+   );
 };
 
 export default ProfileNav;
--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -5,19 +5,25 @@ import Role from "../components/Profile/Role";
 import Security from "../components/Profile/Security";
 
 const Profile = () => {
-  const [isProfile, setIsProfile] = useState(false);
-  return (
-    <>
-      <div className="flex  gap-4 mt-8 px-32">
-        <ProfileNav isProfile={isProfile} setIsProfile={setIsProfile} />
-        <div className="flex flex-col gap-4">
-          {isProfile && <EditProfile />}
-          <Security />
-          <Role />
-        </div>
-      </div>
-    </>
-  );
+   const [isProfile, setIsProfile] = useState(false);
+   const [isSecurity, setIsSecurity] = useState(false);
+   return (
+      <>
+         <div className="flex  gap-4 mt-8 px-32">
+            <ProfileNav
+               isProfile={isProfile}
+               setIsProfile={setIsProfile}
+               isSecurity={isSecurity}
+               setIsSecurity={setIsSecurity}
+            />
+            <div className="flex flex-col gap-4">
+               {isProfile && <EditProfile />}
+               {isSecurity && <Security />}
+               <Role />
+            </div>
+         </div>
+      </>
+   );
 };
 
 export default Profile;
