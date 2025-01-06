59bec5266e2a5b739cbd92588931897fe656b521
Christo-Jacob <christojacob28@gmail.com>
2025-01-06 14:43:45 +0530

    changes in NavBarShop and Sidebar

--- client/src/components/shopping/BannerShop/Banner.tsx
+++ client/src/components/shopping/BannerShop/Banner.tsx
@@ -1,23 +1,23 @@
 import styles from "./Banner.module.css";
 
 const Banner = () => {
-  return (
-    <div
-      className={`${styles.bannerSection} bg-cover  flex justify-center items-center text-black text-center p-5`}
-    >
-      <div className="rounded-lg p-8 flex flex-col items-start text-center">
-        <span className="text-lg font-semibold">Fashion Sale</span>
-        <h2 className="text-4xl font-bold my-2">Minimal Menz Style</h2>
-        <p className="text-base w-full md:w-1/2">
-          Consectetur adipisicing elit. Laborum fuga incidunt laboriosam
-          voluptas iure, delectus dignissimos facilis neque nulla earum.
-        </p>
-        <button className="bg-purple-700 text-white rounded-md px-5 py-2 mt-4 hover:bg-purple-800 transition">
-          Shop Now
-        </button>
+   return (
+      <div
+         className={`${styles.bannerSection} bg-cover  flex justify-center items-center text-black text-center p-5`}
+      >
+         <div className="rounded-lg p-8 flex flex-col items-start text-center">
+            <span className="text-lg font-semibold">Fashion Sale</span>
+            <h2 className="text-4xl font-bold my-2">Minimal Menz Style</h2>
+            <p className="text-base w-full md:w-1/2">
+               Consectetur adipisicing elit. Laborum fuga incidunt laboriosam
+               voluptas iure, delectus dignissimos facilis neque nulla earum.
+            </p>
+            <button className="bg-purple-700 text-white rounded-md px-5 py-2 mt-4 hover:bg-purple-800 transition">
+               Shop Now
+            </button>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Banner;
--- client/src/components/shopping/NavBarShop/NavBarShop.tsx
+++ client/src/components/shopping/NavBarShop/NavBarShop.tsx
@@ -5,33 +5,24 @@ import { GoHeartFill } from "react-icons/go";
 import { FaShoppingCart } from "react-icons/fa";
 
 const NavBarShop = () => {
-  return (
-    <div className="relative w-full h-10 bg-transparent">
-      <div className="absolute left-20 flex items-center gap-5 h-full">
-        <img
-          src="../../src/assets/shopping/loder.png.webp"
-          alt="Logo"
-          className="w-6 h-6"
-        />
-        <h3 className="font-bold text-xl text-purple-700">FIT MAESTRO</h3>
-      </div>
+   return (
+      <div className="w-full   sticky top-0 py-4 bg-primary-dark flex justify-between px-24">
+         <div className="flex items-center gap-6">
+            <div className="w-8 h-8 bg-secondary-light rounded-full"></div>
+            <h3 className="font-bold text-xl text-primary-light">
+               FIT MAESTRO
+            </h3>
+         </div>
 
-      <div className="absolute right-20 flex items-center gap-5 h-full">
-        <button className="w-5 h-5 text-xl text-black rounded-full">
-          <FiSearch />
-        </button>
-        <button className="w-5 h-5 text-xl text-black rounded-full">
-          <FaCircleUser />
-        </button>
-        <button className="w-5 h-5 text-xl text-black rounded-full">
-          <GoHeartFill />
-        </button>
-        <button className="w-5 h-5 text-xl text-black rounded-full">
-          <FaShoppingCart />
-        </button>
+         <div className="flex items-center gap-6">
+            <FiSearch className="text-3xl text-secondary-light rounded-full" />
+
+            <FaCircleUser className="text-3xl text-secondary-light rounded-full" />
+            <GoHeartFill className="text-3xl text-secondary-light rounded-full" />
+            <FaShoppingCart className="text-3xl text-secondary-light rounded-full" />
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default NavBarShop;
