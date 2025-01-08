9348f40029a3bee4adae837d4a4fea2d94bfa1a5
Christo-Jacob <christojacob28@gmail.com>
2025-01-08 12:49:05 +0530

    changes in ProductCard and ProductCategory

--- client/src/components/shopping/ProductShop/ProductCard.tsx
+++ client/src/components/shopping/ProductShop/ProductCard.tsx
@@ -1,33 +1,34 @@
 import React from "react";
 import { IoIosStar } from "react-icons/io";
+import Button from "../../Button/Button";
 
 const ProductCard = ({ prev }) => {
-  return (
-    <div className="flex flex-col justify-between bg-white rounded-lg overflow-hidden shadow-md hover:scale-105 transform transition-transform duration-200 w-64 m-2">
-      <div className="relative">
-        <img
-          src={prev.image}
-          alt=""
-          className="w-52 h-52 object-cover rounded-t-lg"
-        />
-        <span className="absolute bottom-4 right-2 text-sm bg-white text-black rounded-md px-2 py-1 hover:bg-purple-700 hover:text-white">
-          {prev.rating} <IoIosStar />
-        </span>
+   return (
+      <div className="flex flex-col   rounded-lg overflow-hidden  hover:scale-105 transform transition-transform duration-200 w-60 p-5 m-2">
+         <div className="relative w-full h-60">
+            <img
+               src={prev.image}
+               alt=""
+               className="w-full h-full bg-cover rounded-lg"
+            />
+            <span className="absolute flex bottom-2 right-1 text-sm bg-white text-black rounded-lg px-2 py-1 hover:bg-primary-dark hover:text-white">
+               {prev.rating} <IoIosStar />
+            </span>
+         </div>
+         <span className="font-bold text-lg text-gray-800 truncate px-2">
+            {prev.name}
+         </span>
+         <div className="flex  gap-5 items-center px-2 my-2">
+            <span className="text-lg text-primary-dark">
+               ₹{prev.price.discounted}
+            </span>
+            <span className="text-sm text-gray-500 line-through">
+               ₹{prev.price.original}
+            </span>
+         </div>
+         <Button label={"Add To Cart"} />
       </div>
-      <span className="font-bold text-lg text-gray-800 truncate px-2">
-        {prev.name}
-      </span>
-      <div className="flex justify-between items-center px-2 my-2">
-        <span className="text-lg text-pink-500">₹{prev.price.discounted}</span>
-        <span className="text-sm text-gray-500 line-through">
-          ₹{prev.price.original}
-        </span>
-      </div>
-      <button className="mt-auto bg-purple-700 text-white rounded-md py-2 px-4 m-2 hover:bg-purple-600">
-        Add to Cart
-      </button>
-    </div>
-  );
+   );
 };
 
 export default ProductCard;
--- client/src/components/shopping/ProductShop/ProductCategory.tsx
+++ client/src/components/shopping/ProductShop/ProductCategory.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import ProductCard from "./ProductCard";
 
 const ProductCategory = ({ title, products }) => {
-  return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold uppercase text-gray-800 mb-4">
-        {title}
-      </h2>
-      <div className="flex flex-wrap justify-around">
-        {products.map((prev) => (
-          <ProductCard prev={prev} key={prev.id} />
-        ))}
+   return (
+      <div className="px-28 ">
+         <h2 className="text-2xl font-bold uppercase text-gray-800 mb-4">
+            {title}
+         </h2>
+         <div className="flex flex-wrap justify-around">
+            {products.map((prev) => (
+               <ProductCard prev={prev} key={prev.id} />
+            ))}
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default ProductCategory;
