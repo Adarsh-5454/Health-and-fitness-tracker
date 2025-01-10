fdcc2653e1bfe00b068c09fecd4b893adbe22d5d
Athira-p-g <athirabkm@gmail.com>
2025-01-10 14:58:52 +0530

    changes in chatmain

--- client/src/components/Chat/Chatmain.tsx
+++ client/src/components/Chat/Chatmain.tsx
@@ -7,22 +7,23 @@ import style from "./Chat.module.css";
 function Chatmain() {
   return (
     <>
-      <div className="bg-white px-32">
+      <div className="bg-white px-6 md:px-20 md:h-screen">
         <div>
-          <h3 className="font-bebas font-medium text-2xl sm:text-4xl md:text-5xl text-center pt-16">
+          <h3 className="text-4xl md:text-5xl text-center pt-16">
             CHAT WITH OUR EXPERTS
           </h3>
-          <p className="mt-10 text-lg sm:text-xl md:text-2xl text-center pt-5 font-sans">
+          <p className="text-xl md:text-2xl text-center pt-5 font-sans">
             Connect with our experts to get professional advice on health,
             nutrition, and fitness.
             <br />
             Ask questions, share your concerns, and receive personalized
             guidance to achieve your wellness goals.
           </p>
+          <br />
         </div>
-        <div className="grid grid-cols sm:grid-cols  lg:grid-cols-3 gap-4 sm:gap-6 md:gap-8 p-4 sm:p-6 md:p-10 lg:p-20">
+        <div className="grid grid-cols gap-6 md:grid-cols-3 md:gap-8">
           <div
-            className="flex flex-col justify-center p-4 sm:p-6 md:p-8 border-4 border-black bg-cover bg-center opacity-75 hover:scale-105 transition-transform duration-500"
+            className="flex flex-col justify-center p-6 md:p-8 border-4 border-black bg-cover bg-center opacity-75 hover:scale-105 transition-transform duration-500"
             style={{
               backgroundImage:
                 "url('assets/istockphoto-1396653978-612x612.jpg')",
@@ -30,17 +31,17 @@ function Chatmain() {
               width: "100%",
             }}
           >
-            <h4 className="text-2xl sm:text-3xl md:text-4xl text-center font-bebas text-blue-700 py-4 sm:py-7">
+            <h4 className="text-3xl md:text-4xl text-center text-blue-700 md:py-7 py-4">
               NUTRITIONIST
             </h4>
-            <p className="text-base sm:text-lg md:text-xl text-center font-serif font-medium text-black px-4 sm:px-8">
+            <p className="text-lg md:text-xl text-center font-serif font-medium text-black md:px-4 px-8">
               Receive personalized dietary recommendations from expert
               nutritionists.
             </p>
           </div>
 
           <div
-            className="flex flex-col justify-center p-4 sm:p-6 md:p-8 border-4 border-black bg-cover bg-center opacity-75 hover:scale-105 transition-transform duration-500"
+            className="flex flex-col justify-center p-8 md:p-6 border-4 border-black bg-cover bg-center opacity-75 hover:scale-105 transition-transform duration-500"
             style={{
               backgroundImage:
                 "url('assets/abdominal-male-blue-hoodie-holds-burning-barbell-with-smoke-grey-background_613910-15332.avif')",
@@ -48,16 +49,16 @@ function Chatmain() {
               width: "100%",
             }}
           >
-            <h4 className="text-2xl sm:text-3xl md:text-4xl text-center font-bebas text-blue-700 py-4 sm:py-7">
+            <h4 className="text-3xl md:text-4xl text-center font-bebas text-blue-700 md:py-4 py-7">
               GYM EXPERT
             </h4>
-            <p className="text-base sm:text-lg md:text-xl text-center font-serif font-medium text-black px-4 sm:px-8">
+            <p className="text-lg md:text-xl text-center font-serif font-medium text-black md:px-4 px-8">
               Get fitness tips and workout guidance from certified gym experts.
             </p>
           </div>
 
           <div
-            className="flex flex-col justify-center p-4 sm:p-6 md:p-8 border-4 border-black bg-cover bg-center opacity-75 hover:scale-105 transition-transform duration-500"
+            className="flex flex-col justify-center p-6 md:p-8 border-4 border-black bg-cover bg-center opacity-75 hover:scale-105 transition-transform duration-500"
             style={{
               backgroundImage:
                 "url('assets/portrait-hansome-young-male-doctor-man_171337-5068.avif')",
@@ -65,10 +66,10 @@ function Chatmain() {
               width: "100%",
             }}
           >
-            <h4 className="text-2xl sm:text-3xl md:text-4xl text-center font-bebas text-blue-700 py-4 sm:py-7">
+            <h4 className="text-3xl md:text-4xl text-center font-bebas text-blue-700 md:py-4 py-7">
               DOCTOR
             </h4>
-            <p className="text-base sm:text-lg md:text-xl text-center font-serif font-medium text-black px-4 sm:px-8">
+            <p className="text-lg md:text-xl text-center font-serif font-medium text-black md:px-4 px-8">
               Get reliable medical advice from qualified doctors.
             </p>
           </div>
