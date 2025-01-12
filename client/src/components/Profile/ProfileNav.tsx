a037f2eba38ed1b6ce43084a80ee52ad409ef486
Christo-Jacob <christojacob28@gmail.com>
2025-01-12 19:17:31 +0530

    changes in ProfileNav

--- client/src/components/Profile/ProfileNav.tsx
+++ client/src/components/Profile/ProfileNav.tsx
@@ -11,12 +11,17 @@ const ProfileNav = ({
 
    return (
       <>
-         <div className="flex flex-col pt-8  p-2 sm:pt-8  am:p-5 w-2/12 am:w-56 h-screen ">
+         <div className=" flex flex-col  mx-auto  pt-8  p-2 sm:pt-8  sm:p-5  sm:w-2/12   sm:h-screen ">
             <div className="w-12 h-12 sm:w-36 mx-auto sm:h-36 bg-black rounded-full bg-cover bg-center mb-4 am:mb-8"></div>
-            <Button label={"Edit "} onClick={() => handleEditProfile(0)} />
-            <Button label={"Security"} onClick={() => handleEditProfile(1)} />
-            <Button label={"Role"} onClick={() => handleEditProfile(2)} />
-            <Button label={"Logout"} />
+            <div className="w-fit flex sm:flex-col gap-5 mx-auto sm:w-full sm:w-full">
+               <Button label={"Edit "} onClick={() => handleEditProfile(0)} />
+               <Button
+                  label={"Security"}
+                  onClick={() => handleEditProfile(1)}
+               />
+               <Button label={"Role"} onClick={() => handleEditProfile(2)} />
+               <Button label={"Logout"} />
+            </div>
          </div>
       </>
    );
