542d82f7dd3374950ad8efee3199e2721add2082
Adarsh-5454 <adarshsuresh1192003@gmail.com>
2025-01-13 14:44:56 +0530

    changes in banner

--- client/src/components/shopping/BannerShop/Banner.tsx
+++ client/src/components/shopping/BannerShop/Banner.tsx
@@ -1,13 +1,14 @@
+
 import styles from "./Banner.module.css";
 
 const Banner = () => {
    return (
       <div
-         className={`${styles.bannerSection} bg-cover  flex justify-center items-center text-black text-center p-5`}
+         className={`${styles.bannerSection} bg-cover flex justify-center items-center text-black text-center p-5`}
       >
-         <div className="rounded-lg p-8 flex flex-col items-start text-center">
+         <div className="rounded-lg p-4 md:p-8 flex flex-col items-center md:items-start text-center md:text-left">
             <span className="text-lg font-semibold">Fashion Sale</span>
-            <h2 className="text-4xl font-bold my-2">Minimal Menz Style</h2>
+            <h2 className="text-3xl md:text-4xl font-bold my-2">Minimal Menz Style</h2>
             <p className="text-base w-full md:w-1/2">
                Consectetur adipisicing elit. Laborum fuga incidunt laboriosam
                voluptas iure, delectus dignissimos facilis neque nulla earum.
