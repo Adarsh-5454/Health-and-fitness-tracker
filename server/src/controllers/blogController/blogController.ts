39a492737e709ee45b637d235d62c1dc5fa04ab8
Athira-p-g <athirabkm@gmail.com>
2025-01-14 21:02:32 +0530

    creates new cloudinary for blog images

--- server/src/controllers/blogController/blogController.ts
+++ server/src/controllers/blogController/blogController.ts
@@ -1,25 +1,31 @@
 import { Request, Response } from "express";
 import Blog, { IBlog } from "../../models/blogModel/Blog";
+import { v2 as cloudinary } from "cloudinary";
 
 export const postBlog = async (req: Request, res: Response): Promise<void> => {
-   const { title, discription, image, author, category, createdAt } = req.body;
+  const { title, discription, image, author, category, createdAt } = req.body;
 
-   // Input validation
+  // Input validation
 
-   try {
-      const createBlog: IBlog = new Blog({
-         title,
-         discription,
-         image,
-         category,
-         author,
-         createdAt,
-      });
+  try {
+    const createBlog: IBlog = new Blog({
+      title,
+      discription,
+      image,
+      category,
+      author,
+      createdAt,
+    });
 
-      const savedBlog = await createBlog.save();
-      res.status(201).json({ message: "Blog created successfully", savedBlog });
-   } catch (error) {
-      console.error("Error posting blog:", error);
-      res.status(500).json({ message: "Error posting blog" });
-   }
+    const savedBlog = await createBlog.save();
+    res.status(201).json({ message: "Blog created successfully", savedBlog });
+  } catch (error) {
+    console.error("Error posting blog:", error);
+    res.status(500).json({ message: "Error posting blog" });
+  }
 };
+cloudinary.config({
+  cloud_name: "dcklufz4q",
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
