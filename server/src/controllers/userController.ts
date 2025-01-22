8bbb5bab5ef442ba48e0fb9babe1c01bb5207945
Christo-Jacob <christojacob28@gmail.com>
2025-01-22 11:02:48 +0530

    demo

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,24 +1,30 @@
-import { ReqRes } from '../types/express'
-import User from '../models/User'
+import { Request, Response } from "express";
+import User from "../models/User";
 
-export const getAllUsers = async ({ req, res }: ReqRes): Promise<void> => {
-  try {
-    const users = await User.find()
-    res.json(users)
-  } catch (error) {
-    res.status(500).json({ message: 'Server Error' })
-  }
-}
+export const getAllUsers = async (
+   req: Request,
+   res: Response
+): Promise<void> => {
+   try {
+      const users = await User.find();
+      res.json(users);
+   } catch (error) {
+      res.status(500).json({ message: "Server Error" });
+   }
+};
 
-export const createUser = async ({ req, res }: ReqRes): Promise<void> => {
-  try {
-    const { name, email, password } = req.body
+export const createUser = async (
+   req: Request,
+   res: Response
+): Promise<void> => {
+   try {
+      const { name, email, password } = req.body;
 
-    const user = new User({ name, email, password })
-    const savedUser = await user.save()
+      const user = new User({ name, email, password });
+      const savedUser = await user.save();
 
-    res.status(201).json(savedUser)
-  } catch (error) {
-    res.status(400).json({ message: 'Error creating user' })
-  }
-}
+      res.status(201).json(savedUser);
+   } catch (error) {
+      res.status(400).json({ message: "Error creating user" });
+   }
+};
