e822edabdff9474288825c2f3d5f6dfb7ea83098
Christo-Jacob <christojacob28@gmail.com>
2025-01-24 11:53:54 +0530

    creates statics method for User signup

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,17 +1,26 @@
-import mongoose, { Schema, Document } from 'mongoose'
+import mongoose, { Schema, Document, Model } from "mongoose";
 
 export interface IUser extends Document {
-  name: string
-  email: string
-  password: string
+   name: string;
+   email: string;
+   password: string;
 }
 
 const userSchema: Schema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-})
+   name: { type: String, required: true },
+   email: { type: String, required: true, unique: true },
+   password: { type: String, required: true },
+});
 
-const User = mongoose.model<IUser>('User', userSchema)
+interface IUserModel extends Model<IUser> {
+   signup(name: String, email: String, password: String): Promise<void>;
+}
 
-export default User
+userSchema.statics.signup = async function (
+   name: String,
+   email: String,
+   password: String
+): Promise<void> {};
+
+const User = mongoose.model<IUser>("User", userSchema);
+export default User;
