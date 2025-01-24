c31a1d96e07c573c4d454684d31aea0bbe5e103a
SreekuttanCS <sreekuttancs26@gmail.com>
2025-01-24 11:58:48 +0530

    changes in user model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,26 +1,35 @@
 import mongoose, { Schema, Document, Model } from "mongoose";
 
 export interface IUser extends Document {
-   name: string;
-   email: string;
-   password: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
 const userSchema: Schema = new Schema({
-   name: { type: String, required: true },
-   email: { type: String, required: true, unique: true },
-   password: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
-
+//static method for sign up
 interface IUserModel extends Model<IUser> {
-   signup(name: String, email: String, password: String): Promise<void>;
+  signup(name: String, email: String, password: String): Promise<void>;
 }
 
 userSchema.statics.signup = async function (
-   name: String,
-   email: String,
-   password: String
-): Promise<void> {};
+  name: String,
+  email: String,
+  password: String
+): Promise<void> {
+  try {
+    const exists = await this.findOne({ email });
+    if (exists) {
+      throw new Error("Email is already existed");
+    }
+  } catch (error) {
+    throw error;
+  }
+};
 
 const User = mongoose.model<IUser>("User", userSchema);
 export default User;
