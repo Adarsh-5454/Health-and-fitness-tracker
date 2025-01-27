1727c045a85c27f157fc6fbb7f8eb05544fcf418
SreekuttanCS <sreekuttancs26@gmail.com>
2025-01-27 20:12:11 +0530

    changes in shopping

--- server/src/models/shoppingModel/productModel.ts
+++ server/src/models/shoppingModel/productModel.ts
@@ -1,41 +1,41 @@
-import mongoose, { Schema, Document } from "mongoose";
+  import mongoose, { Schema, Document } from "mongoose";
 
-export interface IProduct extends Document {
-  name: string;
-  image: string;
-  price: {
-    original: number;
-    discounted: number;
-  };
-  rating: number;
-  category: string;
-  specifications: {
-    color: string;
-    material: string;
-    product_dimensions: string;
-    pattern: string;
-    sleeve: string;
-    size: string;
-  };
-}
+  export interface IProduct extends Document {
+    name: string;
+    image: string;
+    price: {
+      original: number;
+      discounted: number;
+    };
+    rating: number;
+    category: string;
+    specifications: {
+      color: string;
+      material: string;
+      product_dimensions: string;
+      pattern: string;
+      sleeve: string;
+      size: string;
+    };
+  }
 
-const productSchema: Schema = new Schema({
-  name: { type: String, required: true },
-  image: { type: String, required: true },
-  price: {
-    original: { type: Number, required: true },
-    discounted: { type: Number, required: true },
-  },
-  rating: { type: Number, required: true, min: 0, max: 5 },
-  category: { type: String, required: true },
-  specifications: {
-    color: { type: String, required: true },
-    material: { type: String },
-    sleeve: { type: String },
-    size: { type: String },
-  },
-});
+  const productSchema: Schema = new Schema({
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    price: {
+      original: { type: Number, required: true },
+      discounted: { type: Number, required: true },
+    },
+    rating: { type: Number, required: true, min: 0, max: 5 },
+    category: { type: String, required: true },
+    specifications: {
+      color: { type: String, required: true },
+      material: { type: String },
+      sleeve: { type: String },
+      size: { type: String },
+    },
+  });
 
-const Product = mongoose.model<IProduct>("Product", productSchema);
+  const Product = mongoose.model<IProduct>("Product", productSchema);
 
-export default Product;
+  export default Product;
