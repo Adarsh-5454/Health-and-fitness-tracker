2680659f1a6e9982616fa0467a427bc384613926
Christo-Jacob <christojacob28@gmail.com>
2025-01-27 21:26:08 +0530

    changes in delete cart

--- server/src/routes/shoppingRoutes/cartRoutes.ts
+++ server/src/routes/shoppingRoutes/cartRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import {
-  addToCart,
-  getCart,
-  updateCart,
-  //   removeFromCart,
+   addToCart,
+   getCart,
+   updateCart,
+   deleteCart,
+   //   removeFromCart,
 } from "../../controllers/shoppingController/cartController";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/", addToCart);
 router.get("/item", getCart);
 router.patch("/item/:id", updateCart);
+router.delete("/item/:id", deleteCart);
 
 export default router;
